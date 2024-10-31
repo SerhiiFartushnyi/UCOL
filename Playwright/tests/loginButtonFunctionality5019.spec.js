331e2ddf4da76c84401977c4fb4db552f318775c
Serhii Fartushnyi <far2shok@gmail.com>
2024-10-31 11:50:59 +0200

    added login button functionallity

---
+++ Playwright/tests/loginButtonFunctionality5019.spec.js
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test';
+const config = require('./config');
+
+//BEGOERE RUNING THE TESTS
+// RUN node tests/saveAuthState.js   to save the authentication state to a file named auth.json
+// RUN npx playwright test tests/loginUcol.spec.js
+
+// Use the saved authentication state
+test.use({ storageState: 'auth.json' });
+
+const mail = config.mail;
+const password = config.password;
+
+// Login Button Functionality
+test('Login button', async ({ page }) => {
+
+    await page.goto('/');
+
+
+    // Visual Test Main Page
+    await expect(page.getByText('Design professional')).toBeVisible();
+    const loginButton = page.locator('#profile').getByRole('paragraph').getByText('log in');
+    await expect(loginButton).toBeVisible();
+    await expect(page.getByText('sign up')).toBeVisible();
+
+    await loginButton.click();
+
+    // Visual Test Login Modal
+    const loginPopup = page.locator('#auth-modal-content');
+    expect(loginPopup).toBeVisible();
+    await expect(loginPopup).toContainText('log in to start creating');
+    await expect(loginPopup).toContainText('No account? Sign up');
+    await expect(page.getByRole('button', { name: 'Log in' })).toBeVisible();
+
+});
