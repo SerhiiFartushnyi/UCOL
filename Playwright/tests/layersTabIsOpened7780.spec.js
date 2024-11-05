154ee311a36f8be162c3c79db6cf0c96cd8ecb08
Serhii Fartushnyi <far2shok@gmail.com>
2024-11-05 14:01:58 +0200

    "Layers tab Opened"

---
+++ Playwright/tests/layersTabIsOpened7780.spec.js
@@ -0,0 +1,49 @@
+import { test, expect } from '@playwright/test';
+import config from './config';
+import { faker } from '@faker-js/faker';
+
+//BEGOERE RUNING THE TESTS
+// RUN node tests/saveAuthState.js   to save the authentication state to a file named auth.json
+// RUN npx playwright test tests/loginUcol.spec.js
+
+// Use the saved authentication state
+
+test.use({ storageState: 'auth.json' });
+
+const mail = config.mail;
+const password = config.password;
+const randomWords = faker.lorem.words();
+
+//Layers Tab Opening 
+
+test ('Layers Tab Opening', async ({ page }) => {
+    test.slow();
+    await page.goto('/');
+
+    await page.locator('#profile').getByRole('paragraph').getByText('log in').click();
+    await page.getByPlaceholder('enter your e-mail address').click();
+    await page.getByPlaceholder('enter your e-mail address').fill(mail);
+    await page.getByRole('button', { name: 'Log in' }).click();
+
+    await page.getByPlaceholder('8 char. +1 symbol, number,').click();
+    await page.getByPlaceholder('8 char. +1 symbol, number,').fill(password);
+    await page.getByRole('button', { name: 'Log in' }).click();
+
+    //Assertions to check if the user is logged in
+    await page.waitForSelector('body');
+    await expect(page.locator('body')).toContainText('Design professional');
+
+    await page.waitForLoadState('networkidle');
+
+    // Go To Scene Tab
+    await page.getByText('features', { exact: true }).click();
+    await page.getByRole('link', { name: 'format extender' }).click();
+
+    await page.getByRole('link', { name: 'start now' }).first().click();
+    
+    //await page.goto('/tool/studio/');
+
+    await page.locator('#layers').click();
+    await expect(page.locator('.genre-assets-topbar--title')).toContainText('layers');
+
+});
